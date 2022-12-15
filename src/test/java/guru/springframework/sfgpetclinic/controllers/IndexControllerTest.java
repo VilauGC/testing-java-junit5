27a4f6e81d03228cbfffa604bc212664b70a2012
VilauGC <vilau.gc@gmail.com>
2022-12-15 15:10:17 +0200

    Learned about Enabled Os and Enabled Environment Variable. Some test will run only if the OS is the one in the annotation and also if the environment variable exists.

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgpetclinic.model.ValueNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
@@ -47,6 +48,42 @@ class IndexControllerTest {
     void testAssumptionTrueAssumtionIsTrue(){
 
         assumeTrue("GURU".equalsIgnoreCase("guru"));
+
+    }
+
+    @EnabledOnOs(OS.MAC)
+    @Test
+    void testMeOnMacOS(){
+        System.out.println("TESTING ON MAC OS");
+    }
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testMeOnWindowsOS(){
+        System.out.println("TESTING ON Windows");
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testMeOnJava8(){
+        System.out.println("Testing on java 8");
+    }
+
+    @EnabledOnJre(JRE.JAVA_11)
+    @Test
+    void testMeOnJava11(){
+        System.out.println("Testing on java 11");
+    }
+
+    @EnabledIfEnvironmentVariable(named="USER", matches = "Ciprian")
+    @Test
+    void testIfUserCiprian(){
+        System.out.println("USER is Ciprian in env variables");
+    }
+
+    @EnabledIfEnvironmentVariable(named="USER", matches = "Fred")
+    @Test
+    void testIfUserFred(){
 
     }
 
